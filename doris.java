1219bc8be74d8ccc15391c05a619006f2b743dea
mugo-doris <mugodoris5@gmail.com>
2025-02-07 00:10:25 +0300

    Update doris.java

--- doris.java
+++ doris.java
@@ -1,21 +1,30 @@
+//Name: Doris Nduta
+//Reg no: CT101/G/21006/23
+//Assignment3:
+
+/*java program accepting user input*/ 
 import java.util.Scanner;
 
 public class doris {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
 
+        //name input and output
         System.out.println("Enter name:");
         String name = myScanner.nextLine();
         System.out.println("Name is:" + name);
 
+        //age input and output
         System.out.println("Enter age:");
         int age = myScanner.nextInt();
         System.out.println("Age is:" + age);
 
+         //salary input and output
         System.out.println("Enter salary:");
         double salary = myScanner.nextDouble();
         System.out.println("Salary is:" + salary);
 
+         //boolean expression input and output
         System.out.println("Working (true/false):");
         boolean working = myScanner.nextBoolean();
         System.out.println("Working status:" + working);
